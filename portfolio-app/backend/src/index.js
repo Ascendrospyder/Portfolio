f1e8ab2f799621218aa1fd39be46a2a4fb397462
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2023-12-01 02:12:55 +1100

    Figured out how to send an email by creating my own backend and using nodeemailer

---
+++ portfolio-app/backend/src/index.js
@@ -0,0 +1,50 @@
+require('dotenv').config({ path: '../.env' });
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/send-email', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_APP_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: 'spyderascendro@gmail.com',
+    subject: 'New Contact Form Submission',
+    html: `
+      <h3>New Contact Form Submission</h3>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Message:</strong></p>
+      <p>${message}</p>
+    `,
+  };
+
+  try {
+    // Send email
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('Email sent successfully!');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).send('Error sending email.');
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- portfolio-app/src/Components/Contact/ContactForm.jsx
+++ portfolio-app/src/Components/Contact/ContactForm.jsx
@@ -1,98 +1,190 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Container, Grid, TextField, Button, Paper } from '@mui/material';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import Snackbar from '@mui/material/Snackbar';
 
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [emailError, setEmailError] = useState(false);
+  const [confirmationOpen, setConfirmationOpen] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(formData.email)) {
+      setEmailError(true);
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:3001/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log('Passed to backend successfully!');
+        setFormData({ name: '', email: '', message: '' });
+        setConfirmationOpen(true);
+      }
+    } catch (e) {
+      console.error('Error submitting form:', e);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    if (e.target.name === 'email' && validateEmail(e.target.value)) {
+      setEmailError(false);
+    }
+  };
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const handleCloseConfirmation = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setConfirmationOpen(false);
+  };
+
   return (
     <Container
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-          }}
-        >
-          <Paper
-            elevation={3}
-            sx={{
-              width: '100%',
-              padding: '2rem',
-              borderRadius: '20px',
-              bgcolor: 'common.white',
-            }}
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          width: '100%',
+          padding: '2rem',
+          borderRadius: '20px',
+          bgcolor: 'common.white',
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                label='Name or Handle'
+                variant='outlined'
+                sx={{
+                  width: '100%',
+                  borderRadius: '10px',
+                  '& .MuiInputLabel-root': {
+                    color: 'black',
+                    fontWeight: 'bold',
+                  },
+                }}
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+                name='name'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label='Email'
+                variant='outlined'
+                sx={{
+                  width: '100%',
+                  borderRadius: '10px',
+                  '& .MuiInputLabel-root': {
+                    color: 'black',
+                    fontWeight: 'bold',
+                  },
+                }}
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+                name='email'
+                error={emailError}
+                helperText={emailError ? 'Please enter a valid email' : ''}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label='Message'
+                variant='outlined'
+                multiline
+                rows={4}
+                sx={{
+                  width: '100%',
+                  borderRadius: '10px',
+                  '& .MuiInputLabel-root': {
+                    color: 'black',
+                    fontWeight: 'bold',
+                  },
+                }}
+                value={formData.message}
+                onChange={handleInputChange}
+                required
+                name='message'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant='contained'
+                sx={{
+                  width: '100%',
+                  bgcolor: '#111827',
+                  color: 'common.white',
+                  '&:hover': {
+                    bgcolor: 'common.white',
+                    color: '#111827',
+                  },
+                }}
+                size='large'
+                type='submit'
+              >
+                Send Message{' '}
+                <MailOutlineIcon fontSize='small' sx={{ marginLeft: '5px' }} />
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={confirmationOpen}
+        autoHideDuration={6000}
+        onClose={handleCloseConfirmation}
+        message='Yay, you sent an email to me!'
+        action={
+          <Button
+            color='inherit'
+            size='small'
+            onClick={handleCloseConfirmation}
           >
-            <form>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    label='Name or Handle'
-                    variant='outlined'
-                    sx={{
-                      width: '100%',
-                      borderRadius: '10px',
-                      '& .MuiInputLabel-root': {
-                        color: 'black',
-                        fontWeight: 'bold',
-                      },
-                    }}
-                    required
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label='Email'
-                    variant='outlined'
-                    sx={{
-                      width: '100%',
-                      borderRadius: '10px',
-                      '& .MuiInputLabel-root': {
-                        color: 'black',
-                        fontWeight: 'bold',
-                      },
-                    }}
-                    required
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label='Message'
-                    variant='outlined'
-                    multiline
-                    rows={4}
-                    sx={{
-                      width: '100%',
-                      borderRadius: '10px',
-                      '& .MuiInputLabel-root': {
-                        color: 'black',
-                        fontWeight: 'bold',
-                      },
-                    }}
-                    required
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    variant='contained'
-                    sx={{
-                      width: '100%',
-                      bgcolor: '#111827',
-                      color: 'common.white',
-                      '&:hover': {
-                        bgcolor: 'common.white',
-                        color: '#111827',
-                      },
-                    }}
-                    size='large'
-                    type='submit'
-                  >
-                    Send Message <MailOutlineIcon fontSize='small' sx={{ marginLeft: '5px' }} />
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
-          </Paper>
-        </Container>
-  )
-}
+            Close
+          </Button>
+        }
+      />
+    </Container>
+  );
+};
 
 export default ContactForm;
