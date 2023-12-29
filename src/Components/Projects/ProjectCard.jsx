cb06d347caf9620de3c0d7d2b388ed0115ad54c1
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2023-12-29 17:39:16 +1100

    Made the projects page responsive

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -1,55 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './projects.scss';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 
-const ProjectCard = ({ title, description, image, projectLink }) => {
+const ProjectCard = ({ title, description, image, projectLink, languages }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowFullDescription(!showFullDescription);
+  };
+
   const handleProjectLinkClick = () => {
     window.open(projectLink, '_blank'); // Open the link in a new tab
   };
 
   return (
-    <Card
-      sx={{
-        width: 345,
-        height: 500,
-        margin: '5rem 1rem 1rem 1rem',
-        borderRadius: '10%',
-        transition: 'transform 0.3s ease-in-out',
-        '&:hover': {
-          transform: 'translateY(-10px)',
-        },
+    <div
+      style={{
+        width: '300px',
+        border: '1px solid #ccc',
+        borderRadius: '8px',
+        padding: '16px',
       }}
-      className='project-card'
     >
-      <CardMedia sx={{ height: 140 }} image={image} title='green iguana' />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant='h5'
-          component='div'
-          sx={{ color: 'black', fontWeight: 700 }}
+      <img
+        src={image}
+        alt='Card'
+        style={{
+          maxWidth: '500px',
+          maxHeight: '100px',
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <h2 style={{ marginTop: '8px' }}>{title}</h2>
+      <hr style={{ margin: '8px 0' }} />
+      <p style={{ fontSize: '1rem', whiteSpace: 'pre-wrap' }}>
+        {showFullDescription ? description : `${description.substring(0, 100)}...`}
+        {!showFullDescription && (
+          <button
+            onClick={toggleDescription}
+            style={{
+              border: 'none',
+              backgroundColor: 'transparent',
+              color: '#007bff',
+              cursor: 'pointer',
+            }}
+          >
+            Read more
+          </button>
+        )}
+      </p>
+      {showFullDescription && (
+        <button
+          onClick={toggleDescription}
+          style={{
+            border: 'none',
+            backgroundColor: 'transparent',
+            color: '#007bff',
+            cursor: 'pointer',
+          }}
         >
-          {title}
-        </Typography>
-        <Typography variant='body2' color='text.secondary'>
-          {description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button
-          size='small'
-          onClick={handleProjectLinkClick}
-          sx={{ marginLeft: '5px' }}
-        >
-          Link to project
-        </Button>
-      </CardActions>
-    </Card>
+          Show less
+        </button>
+      )}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginTop: '8px',
+        }}
+      >
+        {languages.map((language, index) => (
+          <div
+            key={index}
+            style={{
+              border: '1px solid #007bff',
+              color: 'lightblue',
+              borderRadius: '4px',
+              padding: '4px 8px',
+              marginRight: '4px',
+              display: 'inline-block',
+              fontSize: '0.8rem',
+            }}
+          >
+            {language}
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={handleProjectLinkClick}
+        style={{
+          marginTop: '20px',
+          padding: '8px 16px',
+          borderRadius: '4px',
+          backgroundColor: '#007bff',
+          color: 'white',
+          border: 'none',
+          fontSize: '1rem',
+          cursor: 'pointer',
+          transition: 'background-color 0.3s ease-in-out',
+        }}
+      >
+        View Project
+      </button>
+    </div>
   );
 };
 
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React from 'react';
 import SkillCard from './SkillCard';
 import CLogo from '../Skills/images/c-logo.jpg';
-import JavaLogo from '../Skills/images/java-logo.jpg'
-import JavascriptLogo from '../Skills/images/JS-logo.png'
-import TypescriptLogo from '../Skills/images/TS-logo.png'
-import PythonLogo from '../Skills/images/py-logo.png'
-import PostgresLogo from '../Skills/images/postgres-logo.png'
-import ReactLogo from '../Skills/images/react-logo.png'
-import ReactNativeLogo from '../Skills/images/react-native-logo.png'
-import GitLogo from '../Skills/images/git-logo.png'
-import UIUXLogo from '../Skills/images/ui-ux-logo.jpg'
+import JavaLogo from '../Skills/images/java-logo.jpg';
+import JavascriptLogo from '../Skills/images/JS-logo.png';
+import TypescriptLogo from '../Skills/images/TS-logo.png';
+import PythonLogo from '../Skills/images/py-logo.png';
+import PostgresLogo from '../Skills/images/postgres-logo.png';
+import ReactLogo from '../Skills/images/react-logo.png';
+import ReactNativeLogo from '../Skills/images/react-native-logo.png';
+import GitLogo from '../Skills/images/git-logo.png';
+import UIUXLogo from '../Skills/images/ui-ux-logo.jpg';
 
 const Skills = () => {
- return (
-  <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
-    <SkillCard image={CLogo} title="C" />
-    <SkillCard image={JavaLogo} title="Java" />
-    <SkillCard image={JavascriptLogo} title="JavaScript" />
-    <SkillCard image={TypescriptLogo} title="TypeScript" />
-    <SkillCard image={PythonLogo} title="Python" />
-    <SkillCard image={PostgresLogo} title="PostgreSQl" />
-    <SkillCard image={ReactLogo} title="ReactJS" />
-    <SkillCard image={ReactNativeLogo} title="React Native" />
-    <SkillCard image={GitLogo} title="Git" />
-    <SkillCard image={UIUXLogo} title="UI/UX Design" />
-  </div>
- )
-}
+  return (
+    <div
+      className='skills-container'
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        maxHeight: 'calc(100vh - 100px)',
+        overflow: 'auto',
+        padding: '20px',
+      }}
+    >
+      <SkillCard image={CLogo} title='C' />
+      <SkillCard image={JavaLogo} title='Java' />
+      <SkillCard image={JavascriptLogo} title='JavaScript' />
+      <SkillCard image={TypescriptLogo} title='TypeScript' />
+      <SkillCard image={PythonLogo} title='Python' />
+      <SkillCard image={PostgresLogo} title='PostgreSQl' />
+      <SkillCard image={ReactLogo} title='ReactJS' />
+      <SkillCard image={ReactNativeLogo} title='React Native' />
+      <SkillCard image={GitLogo} title='Git' />
+      <SkillCard image={UIUXLogo} title='UI/UX Design' />
+    </div>
+  );
+};
 
 export default Skills;
